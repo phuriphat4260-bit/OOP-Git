ce12bce700927b68e8f5aa3f740b6e104aba730e
Atigorn.S <ati2rok@gmail.com>
2025-12-23 14:23:31 +0700

    create class EV

--- main.py
+++ main.py
@@ -6,5 +6,18 @@ class Car:
     def drive(self):
         print (f'{self.brand} , {self.model} is driving')
 
+class ElectricCar(Car):
+    def __init__(self, model, brand, battery_size):
+        # ใช้ super() เพื่อดึงคุณสมบัติจากคลาสแม่ (Car)
+        super().__init__(model, brand)
+        self.battery_size = battery_size
+
+    def charge(self):
+        print(f"The {self.battery_size}kWh battery is charging...")
+
 my_car = Car('Toyota', 'Vios')
 my_car.drive()
+my_ev = ElectricCar('Tesla', 'Model 3', 75)
+my_ev.drive()
+my_ev.charge()
+
